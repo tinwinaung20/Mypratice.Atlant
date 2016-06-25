e5a31822c6732cb76f27bb9f71eb073b97d8f3b9
TinWin <tinwin1987@gmail.com>
2016-06-25 09:47:19 +0800

    //Add Unit Test

---
+++ Atlant.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlant.Model
+{
+    public class Class1
+    {
+    }
+}
